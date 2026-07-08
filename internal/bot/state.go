c279820b7fa16a645036c9d81c6747490823f1d4
sunnng <61936492+sunnng@users.noreply.github.com>
2026-07-08 14:17:49 +0800

    feat(bot): add state interface and registry

--- internal/bot/state.go
+++ internal/bot/state.go
@@ -7,3 +7,24 @@ type State interface {
 	Next(ctx *Context) State
 	Recover(ctx *Context) error
 }
+
+type Registry struct {
+	states []State
+}
+
+func NewRegistry() *Registry {
+	return &Registry{}
+}
+
+func (r *Registry) Register(s State) {
+	r.states = append(r.states, s)
+}
+
+func (r *Registry) Find(ctx *Context) State {
+	for _, s := range r.states {
+		if s.Detect(ctx) {
+			return s
+		}
+	}
+	return nil
+}
